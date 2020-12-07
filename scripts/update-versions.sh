8773ce7fb2cf7dc1b43c3311556c0d6104d4f06b
Devlin Junker <devlin.junker@gmail.com>
2020-12-06 17:00:21 -0800

    feature: working update-versions.sh script

--- scripts/update-versions.sh
+++ scripts/update-versions.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-
 # TODO: Open Special files and increment version
+
 
 ## README
 #   - Replace first 3 lines with:
@@ -8,3 +8,17 @@
 #   ---
 #   Version: @1
 #   ---
+
+if [ -z $1 ]; then
+    echo "Error: no version number"
+    exit -1;
+fi;
+
+if [ -f ./README.md ]; then
+    cp README.md README.old.md;
+    echo "---
+Version: $1
+---" > README.md; 
+    tail -n +4 ./README.old.md >> README.md
+    rm README.old.md
+fi
