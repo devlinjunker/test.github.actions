140b588e5731f908a648976b6855d492c6c7a0a0
Devlin Junker <devlin.junker@gmail.com>
2020-12-13 13:18:42 -0800

    feature: separate job for upmerge after release

--- scripts/release-prep-upmerge.sh
+++ scripts/release-prep-upmerge.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
 
 git checkout develop;
+
+# Get version in `develop` (We wantt this to be version in the final README)
+VERSION=`less README.md | head -n 3`;
+
+# upmerge from main
 git pull;
 git merge main;
-git reset README.md;
+
+# TODO: Resolve conflicts better (maybe https://github.com/jakub-g/git-resolve-conflict)
 git checkout README.md;
